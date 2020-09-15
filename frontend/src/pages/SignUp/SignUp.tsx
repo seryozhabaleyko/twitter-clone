8b5f0d097d42b2b93c4a0f1113636df5e0b1c582
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-15 12:41:59 +0300

    Create SignUp.tsx

---
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SignUp.scss';
+
+function SignUp() {
+    return (
+        <main className="main">
+            <div>SignUp</div>
+        </main>
+    );
+}
+
+export default SignUp;
