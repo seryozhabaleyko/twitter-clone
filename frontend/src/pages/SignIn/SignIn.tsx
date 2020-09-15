dc0bc3f600a7d5b932b2495610dc46ce1e19e6ec
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-15 12:41:52 +0300

    Create SignIn.tsx

---
+++ frontend/src/pages/SignIn/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SignIn.scss';
+
+function SignIn() {
+    return (
+        <main className="main">
+            <div>SignIn</div>
+        </main>
+    );
+}
+
+export default SignIn;
