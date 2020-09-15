574b01d45b6971b7b60aeca3f2a71ddb19eddc88
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-15 12:41:46 +0300

    Create Logout.tsx

---
+++ frontend/src/pages/Logout/Logout.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Logout.scss';
+
+function Logout() {
+    return (
+        <main className="main logout-page">
+            <section className="login-side">login-side</section>
+            <section className="info-side">info-side</section>
+        </main>
+    );
+}
+
+export default Logout;
