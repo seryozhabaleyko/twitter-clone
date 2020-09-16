d2a129f08d7ce50263667423a674cb1c683f2d15
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-16 20:57:41 +0300

    Update SignUp.tsx

--- frontend/src/pages/SignUp/SignUp.tsx
+++ frontend/src/pages/SignUp/SignUp.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import useDocumentTitle from '../../hooks/useDocumentTitle';
 
 import './SignUp.scss';
 
-function SignUp() {
+function SignUp(): React.ReactElement {
+    useDocumentTitle('Зарегистрироваться в Твиттере / Твиттер');
+
     return (
         <main className="main">
             <div>SignUp</div>
