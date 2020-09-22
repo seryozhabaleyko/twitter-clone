ed0eeedaf222f656269315562bb6969d9f784d33
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-22 22:59:37 +0300

    Update Tweet.tsx

--- frontend/src/components/Tweet/Tweet.tsx
+++ frontend/src/components/Tweet/Tweet.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Avatar from 'antd/lib/avatar';
+import { UserOutlined } from '@ant-design/icons';
+
+import Header from './Header';
+import Footer from './Footer';
 
 import './Tweet.scss';
 
 function Tweet() {
-    return <div>Tweet</div>;
+    return (
+        <article className="tweet">
+            <div className="tweet__inner">
+                <div className="tweet__avatar">
+                    <Avatar size={49} icon={<UserOutlined />} />
+                </div>
+                <div className="tweet__content">
+                    <Header />
+                    <div className="tweet__text">
+                        <div className="txt">
+                            Оказывается кто-то запутался в своем же коде и рандомно именовал
+                            переменную penguinSize как pinguinSize, попутно её определив и просто,
+                            использовав то одно название, то другое при одинаковой логике. Ох
+                            сколько времени на это ушло
+                        </div>
+                    </div>
+                    <Footer />
+                </div>
+            </div>
+        </article>
+    );
 }
 
 export default Tweet;
