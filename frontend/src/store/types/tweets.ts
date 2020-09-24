c1c70f25ebe165c52d4b3a702a7c0c51fe9e0e55
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-24 17:45:34 +0300

    Update tweets.ts

--- frontend/src/store/types/tweets.ts
+++ frontend/src/store/types/tweets.ts
@@ -5,7 +5,15 @@ import {
     fetchTweetsFailure,
 } from '../actions/tweets';
 
-export interface Tweet {}
+export interface Tweet {
+    _id: string;
+    text: string;
+    user: {
+        fullname: string;
+        username: string;
+        avatarUrl: string;
+    };
+}
 export type Tweets = Tweet[];
 
 export interface TweetsState {
