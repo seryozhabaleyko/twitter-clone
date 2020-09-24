c8aae8a74a0f738626f79cc3ce496c0049a3552d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-24 17:45:59 +0300

    Create tweet.ts

---
+++ frontend/src/store/reducers/tweet.ts
@@ -0,0 +1,38 @@
+import {
+    TweetState,
+    TweetsActionTypes,
+    ADD_TWEET_REQUEST,
+    ADD_TWEET_SUCCESS,
+    ADD_TWEET_FAILURE,
+    FETCH_TWEET_REQUEST,
+    FETCH_TWEET_SUCCESS,
+    FETCH_TWEET_FAILURE,
+} from '../types/tweet';
+
+const initialState: TweetState = {
+    loading: false,
+    data: {
+        id: '',
+        text: '',
+        user: {
+            name: '',
+            nickname: '',
+            avatarUrl: '',
+        },
+    },
+    error: null,
+};
+
+export function tweetReducer(state = initialState, action: TweetsActionTypes) {
+    switch (action.type) {
+        case FETCH_TWEET_REQUEST:
+            return { ...state, loading: true, data: initialState.data, error: null };
+        case FETCH_TWEET_SUCCESS:
+            return { ...state, loading: false, data: action.payload };
+        case FETCH_TWEET_FAILURE:
+            return { ...state, loading: false, error: action.payload };
+
+        default:
+            return state;
+    }
+}
