f3a8f07cd9418a03ebd0a4026e53c37671c6d79d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-24 17:45:48 +0300

    Update tweetsSaga.ts

--- frontend/src/store/sagas/tweetsSaga.ts
+++ frontend/src/store/sagas/tweetsSaga.ts
@@ -1,19 +1,18 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { FETCH_TWEETS } from '../types/tweets';
 import { fetchTweetsRequest, fetchTweetsSuccess, fetchTweetsFailure } from '../actions/tweets';
+import { fetchTweetsApi } from '../../services/api';
 
 function* fetchTweets() {
     try {
         yield put(fetchTweetsRequest());
-        const tweets = yield call(() => {});
-        yield put(fetchTweetsSuccess(tweets));
+        const tweets = yield call(fetchTweetsApi);
+        yield put(fetchTweetsSuccess(tweets.data));
     } catch (error) {
         yield put(fetchTweetsFailure(error));
     }
 }
 
-function* tweetsSaga() {
+export function* tweetsSaga() {
     yield takeEvery(FETCH_TWEETS, fetchTweets);
 }
-
-export default tweetsSaga;
