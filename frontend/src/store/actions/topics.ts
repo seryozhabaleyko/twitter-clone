91bac66cd11ad3f92fae149bd8b505831cab96ec
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-24 17:46:11 +0300

    Create topics.ts

---
+++ frontend/src/store/actions/topics.ts
@@ -0,0 +1,33 @@
+import {
+    FETCH_TOPICS,
+    FETCH_TOPICS_REQUEST,
+    FETCH_TOPICS_SUCCESS,
+    FETCH_TOPICS_FAILURE,
+    Topics,
+} from './../types/topics';
+
+export function fetchTopics() {
+    return {
+        type: FETCH_TOPICS,
+    } as const;
+}
+
+export function fetchTopicsRequest() {
+    return {
+        type: FETCH_TOPICS_REQUEST,
+    } as const;
+}
+
+export function fetchTopicsSuccess(topics: Topics) {
+    return {
+        type: FETCH_TOPICS_SUCCESS,
+        payload: topics,
+    } as const;
+}
+
+export function fetchTopicsFailure(error: Error | null) {
+    return {
+        type: FETCH_TOPICS_FAILURE,
+        payload: error,
+    } as const;
+}
