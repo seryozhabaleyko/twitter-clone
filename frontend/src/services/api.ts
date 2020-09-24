1300f3e7d7883336ca90246bb8da93b136094ab8
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-24 17:46:14 +0300

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,17 @@
 import axios from 'axios';
+import { Tweet } from '../store/types/tweet';
 
-export async function fetchTweetsApi() {
-    const tweets = await axios.get('https://trycode.pw/c/DOLTL.json');
-    return tweets;
+export function fetchTweetsApi() {
+    const url = 'https://trycode.pw/c/2OBQ1.json';
+    return axios.get(url);
+}
+
+export function fetchTopicsApi() {
+    const url = 'https://trycode.pw/c/CT7FO.json';
+    return axios.get(url);
+}
+
+export function addTweetApi(tweet: Tweet) {
+    const url = 'https://trycode.pw/c/2OBQ1.json';
+    return axios.post(url, tweet);
 }
