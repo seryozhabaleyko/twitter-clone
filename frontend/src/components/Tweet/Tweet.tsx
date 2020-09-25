ed647db744296e12044ce09d80b55921f9a2a65b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 01:16:55 +0300

    Update Tweet.tsx

--- frontend/src/components/Tweet/Tweet.tsx
+++ frontend/src/components/Tweet/Tweet.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import Avatar from 'antd/lib/avatar';
-import { UserOutlined } from '@ant-design/icons';
+import Avatar from '@material-ui/core/Avatar';
 
 import { Tweet as TweetProps } from '../../store/types/tweets';
 import Header from './Header';
+import Text from './Text';
+import Photos from './Photos';
 import Footer from './Footer';
 
 import './Tweet.scss';
-import { Link } from 'react-router-dom';
-import Photos from './Photos';
 
-function Tweet({ _id, text, user }: TweetProps) {
+function Tweet({ text, user }: TweetProps) {
     return (
         <article className="tweet">
-            <Link to={`/${user.username}/tweet/${_id}`}>
-                <div className="tweet__inner">
-                    <div className="tweet__avatar">
-                        <Avatar src={user.avatarUrl} size={49} icon={<UserOutlined />} />
-                    </div>
-                    <div className="tweet__content">
-                        <Header {...user} />
-                        <div className="tweet__text">
-                            <div className="txt">{text}</div>
-                        </div>
-                        <Photos />
-                        <Footer />
-                    </div>
+            <div className="tweet__inner">
+                <div className="tweet__avatar">
+                    <Avatar
+                        src={user.avatarUrl}
+                        alt={user.fullname}
+                        style={{ height: 49, width: 49 }}
+                    />
                 </div>
-            </Link>
+                <div className="tweet__content">
+                    <Header {...user} />
+                    <Text text={text} />
+                    <Photos />
+                    <Footer />
+                </div>
+            </div>
         </article>
     );
 }
