62210bda19672f84a55fe57360f4bfc386f29b0d
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-25 17:11:24 +0300

    Update tweet.ts

--- frontend/src/store/sagas/tweet.ts
+++ frontend/src/store/sagas/tweet.ts
@@ -1,20 +1,38 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
-import { AddTweet, ADD_TWEET } from './../types/tweet';
-import { addTweetRequest, addTweetSuccess, addTweetFailure } from '../actions/tweet';
-import { addTweetApi } from '../../services/api';
+import { AddTweet, ADD_TWEET, FetchTweet, FETCH_TWEET } from './../types/tweet';
+import {
+    addTweetRequest,
+    addTweetSuccess,
+    addTweetFailure,
+    fetchTweetRequest,
+    fetchTweetSuccess,
+    fetchTweetFailure,
+} from '../actions/tweet';
+import { addTweetApi, fetchTweetApi } from '../../services/api';
 
 function* addTweet({ tweet }: AddTweet) {
     try {
         yield put(addTweetRequest());
-        const tweetResponse = yield call(addTweetApi, tweet);
-        console.log('tweet response', tweetResponse);
+        const response = yield call(addTweetApi, tweet);
+        console.log('tweet response', response);
 
-        yield put(addTweetSuccess(tweetResponse));
+        yield put(addTweetSuccess(response.data));
     } catch (error) {
         yield put(addTweetFailure(error));
     }
 }
 
+function* fetchTweet({ id }: FetchTweet) {
+    try {
+        yield put(fetchTweetRequest());
+        const tweet = yield call(fetchTweetApi, id);
+        yield put(fetchTweetSuccess(tweet[0]));
+    } catch (error) {
+        yield put(fetchTweetFailure(error));
+    }
+}
+
 export function* tweetSaga() {
     yield takeEvery(ADD_TWEET, addTweet);
+    yield takeEvery(FETCH_TWEET, fetchTweet);
 }
