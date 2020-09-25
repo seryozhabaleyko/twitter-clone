f29cc17e1338843797646878a1ca110b5b32fba9
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 01:16:41 +0300

    Update Topic.tsx

--- frontend/src/components/Topic/Topic.tsx
+++ frontend/src/components/Topic/Topic.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+
 import { Topic as TopicProps } from '../../store/types/topics';
 
 import './Topic.scss';
 
 function Topic({ topic, name, tweet_value }: TopicProps) {
     return (
-        <div className="topic">
+        <div className="topic topic-hover-effect">
             <div className="topic__inner">
                 <div className="topic__topic">Актуальные темы: {topic}</div>
                 <div className="topic__name">{name}</div>
