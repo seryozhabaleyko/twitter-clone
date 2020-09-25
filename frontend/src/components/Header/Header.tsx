0fbc1ff44d846166f569f638b82e4ccef482da0a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-25 17:10:45 +0300

    Update Header.tsx

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,13 +2,38 @@ import React from 'react';
 
 import Logo from '../Logo';
 import Nav from '../Nav';
+import Account from '../Account';
 
 import './Header.scss';
 
 function Header() {
     return (
         <header className="header">
-            <div className="header__inner">
+            <div className="header__container">
+                <div style={{ position: 'fixed', top: '0px', height: '100%' }}>
+                    <div
+                        style={{
+                            overflowY: 'auto',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'space-between',
+                            width: '275px',
+                            height: '100%',
+                            paddingLeft: '10px',
+                            paddingRight: '10px',
+                        }}
+                    >
+                        <div>
+                            <Logo />
+                            <Nav />
+                        </div>
+                        <div>
+                            <Account />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <div className="header__inner">
                 <div
                     style={{
                         position: 'fixed',
@@ -23,11 +48,15 @@ function Header() {
                             height: '100%',
                         }}
                     >
-                        <Logo />
-                        <Nav />
+                        <div>
+                            <Logo />
+                            <Nav />
+                        </div>
+
+                        <Account />
                     </div>
                 </div>
-            </div>
+            </div> */}
         </header>
     );
 }
