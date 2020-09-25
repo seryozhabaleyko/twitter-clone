ae203d361e92dd471d8cfba123a3b39d9acf64ac
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 01:16:30 +0300

    Update Sidebar.tsx

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Search from '../Search';
+import Topics from '../Topics';
+import Subscribe from '../Subscribe';
+
 import './Sidebar.scss';
 
 function Sidebar() {
-    return <div className="sidebar">sidebar</div>;
+    return (
+        <div className="sidebar">
+            <Search />
+            <Topics />
+            <Subscribe />
+        </div>
+    );
 }
 
 export default Sidebar;
