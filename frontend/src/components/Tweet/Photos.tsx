4119d80f563f721e0cc79c0a818d46995f71f90b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-25 17:10:56 +0300

    Create Photos.tsx

---
+++ frontend/src/components/Tweet/Photos.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Photos() {
+    return (
+        <div className="tweet__image">
+            <img src="https://pbs.twimg.com/media/EG0lmlBX4AAdJpY?format=jpg&name=900x900" alt="" />
+        </div>
+    );
+}
+
+export default Photos;
