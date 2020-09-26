d033760970575da2790e82dd85bbe7dafe56a7ed
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:43:34 +0300

    Update rootSaga.ts

--- frontend/src/store/sagas/rootSaga.ts
+++ frontend/src/store/sagas/rootSaga.ts
@@ -1,10 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
+
 import { tweetsSaga } from './tweetsSaga';
 import { topicsSaga } from './topicsSaga';
 import { tweetSaga } from './tweet';
+import { recommendedSaga } from './recommended';
 
 function* rootSaga() {
-    yield all([fork(tweetsSaga), fork(topicsSaga), fork(tweetSaga)]);
+    yield all([fork(tweetsSaga), fork(topicsSaga), fork(tweetSaga), fork(recommendedSaga)]);
 }
 
 export default rootSaga;
