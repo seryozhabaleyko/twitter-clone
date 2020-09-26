53c00dd5627999ea676b200a47be1b31e41934d2
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:41:06 +0300

    Create User.tsx

---
+++ frontend/src/components/Recommended/User.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+
+import { User as UserProps } from '../../store/types/recommended';
+import Follow from './Follow';
+
+import './User.scss';
+
+function User({ name, nickname, avatarUrl }: UserProps) {
+    return (
+        <div role="button" className="recommended-user recommended-hover-effect">
+            <div className="recommended-user__avatar">
+                <Avatar src={avatarUrl} style={{ width: 49, height: 49 }} />
+            </div>
+            <div className="recommended-user__body">
+                <div className="recommended-user__user-info">
+                    <div className="recommended-user__name">{name}</div>
+                    <div className="recommended-user__nickname">@{nickname}</div>
+                </div>
+                <div className="recommended-user__action-button">
+                    <Follow />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default User;
