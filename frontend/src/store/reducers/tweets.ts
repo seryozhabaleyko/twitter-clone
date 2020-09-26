c2a6ff4ef0e6fc073e7ac373465efaf057cbd861
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:43:29 +0300

    Update tweets.ts

--- frontend/src/store/reducers/tweets.ts
+++ frontend/src/store/reducers/tweets.ts
@@ -1,4 +1,3 @@
-import { FETCH_TWEET_SUCCESS } from './../types/tweet';
 import {
     TweetsState,
     TweetsActionTypes,
@@ -21,9 +20,6 @@ export function tweetsReducer(state = initialState, action: TweetsActionTypes): 
             return { ...state, loading: false, data: action.payload };
         case FETCH_TWEETS_FAILURE:
             return { ...state, loading: false, error: action.payload };
-
-        /* case FETCH_TWEET_SUCCESS:
-            return { ...state, data: [...state.data, action.payload] }; */
         default:
             return state;
     }
