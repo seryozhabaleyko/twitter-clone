5a527c3f6da5c3acb400d75a164d89d09e551c39
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:41:00 +0300

    Create Recommended.tsx

---
+++ frontend/src/components/Recommended/Recommended.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+import User from './User';
+import { fetchRecommended } from '../../store/actions/recommended';
+import { getRecommended } from '../../store/selectors/recommended';
+
+import './Recommended.scss';
+
+function Recommended() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchRecommended());
+    }, [dispatch]);
+
+    const { loading, users, error } = useSelector(getRecommended, shallowEqual);
+
+    return (
+        <aside className="recommended" aria-label="Кого читать">
+            {loading && (
+                <div className="recommended__loading">
+                    <CircularProgress />
+                </div>
+            )}
+
+            {!!users.length && (
+                <>
+                    <div className="recommended__header">
+                        <h2 className="recommended__title">Кого читать</h2>
+                    </div>
+                    <div className="recommended__user-list">
+                        {users.map((user) => (
+                            <User key={user.id} {...user} />
+                        ))}
+                    </div>
+                    <Link
+                        to="#"
+                        className="recommended__footer recommended__show-more recommended-hover-effect"
+                    >
+                        Показать еще
+                    </Link>
+                </>
+            )}
+
+            {error && <div className="recommended__error">{error.message}</div>}
+        </aside>
+    );
+}
+
+export default Recommended;
