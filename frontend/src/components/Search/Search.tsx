2e4d3530917073642de18c2db1eae6fb893d0f1e
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:41:12 +0300

    Update Search.tsx

--- frontend/src/components/Search/Search.tsx
+++ frontend/src/components/Search/Search.tsx
@@ -1,5 +1,3 @@
-import { SearchOutlined } from '@ant-design/icons';
-import { Input } from 'antd';
 import React from 'react';
 
 import './Search.scss';
@@ -7,16 +5,18 @@ import './Search.scss';
 function Search() {
     return (
         <div className="search">
-            <div className="search__icon">
-                <svg viewBox="0 0 24 24">
-                    <g>
-                        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                    </g>
-                </svg>
-            </div>
-            <div className="search__input">
-                <input type="text" placeholder="Поиск в Твиттере" />
-            </div>
+            <form className="search-form">
+                <div className="search-form__icon">
+                    <svg viewBox="0 0 24 24">
+                        <g>
+                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                        </g>
+                    </svg>
+                </div>
+                <div className="search-form__input">
+                    <input type="text" placeholder="Поиск в Твиттере" />
+                </div>
+            </form>
         </div>
     );
 }
