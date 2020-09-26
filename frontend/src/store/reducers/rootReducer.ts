f6dacafdd33d51ba38256643f047e2e5a5573bea
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:43:27 +0300

    Update rootReducer.ts

--- frontend/src/store/reducers/rootReducer.ts
+++ frontend/src/store/reducers/rootReducer.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import { tweetsReducer } from './tweets';
 import { topicsReducer } from './topics';
 import { tweetReducer } from './tweet';
+import { recommendedReducer } from './recommended';
 
 const rootReducer = combineReducers({
     tweets: tweetsReducer,
     topics: topicsReducer,
     tweet: tweetReducer,
+    recommended: recommendedReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
