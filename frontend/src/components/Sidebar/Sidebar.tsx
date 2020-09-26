8f038ca248f09696c41d1d717962bc1b3695cd39
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-26 16:41:17 +0300

    Update Sidebar.tsx

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 import Search from '../Search';
 import Topics from '../Topics';
-import Subscribe from '../Subscribe';
+import Recommended from '../Recommended';
+import Footer from '../Footer';
 
 import './Sidebar.scss';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <Search />
-            <Topics />
-            <Subscribe />
+            <div className="sidebar__inner">
+                <Search />
+                <div style={{ height: '53px' }}></div>
+                <Topics />
+                <Recommended />
+                <Footer />
+            </div>
         </div>
     );
 }
