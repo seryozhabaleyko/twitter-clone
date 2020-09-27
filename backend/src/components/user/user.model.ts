9d2d67f8a9bfd1d7055cb510c1d9401368b68375
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-28 01:59:03 +0300

    Create user.model.ts

---
+++ backend/src/components/user/user.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+export const UserModel = model('users', userSchema);
