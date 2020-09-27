9f4b50446118243b8f378744cf9b3de6bd8f13a0
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-28 01:59:07 +0300

    Create server.ts

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+import { app } from './app';
+
+dotenv.config();
+const port = process.env.PORT || '7777';
+
+app.listen(port, (): void => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
