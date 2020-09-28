d8a6d6fac2fffb80bd1790db529562a47810bae6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-29 01:43:46 +0300

    Update user.interface.ts

--- backend/src/components/user/user.interface.ts
+++ backend/src/components/user/user.interface.ts
@@ -1,5 +1,8 @@
-export interface User {
-    _id: string;
+import { Document } from 'mongoose';
+
+export interface IUser extends Document {
     email: string;
     password: string;
 }
+
+export interface IUserDoc extends IUser, Document {}
