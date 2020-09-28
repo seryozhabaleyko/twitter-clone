fa15c507a4f2697a727429ef8273b5b9de7379b4
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-29 01:09:04 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,20 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import { loginRoute, registerRoute } from './components/auth/auth.route';
+import passport from 'passport';
+import authRoute from './components/auth/auth.route';
+// import userRoute from './components/user/user.route';
 import './core/db';
 
 export const app: Application = express();
 
-app.use(morgan('dev'));
-app.use(cors());
+app.use(passport.initialize());
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', loginRoute);
-app.use('/api/auth', registerRoute);
+app.use('/api/auth', authRoute);
+
+// app.use('/api/user', passport.authenticate('jwt', { session: false }), userRoute);
