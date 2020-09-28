2a5e0f8088c3ea4e25cea42d94248fbf9b800de6
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-29 01:09:06 +0300

    Update auth.controller.ts

--- backend/src/components/auth/auth.controller.ts
+++ backend/src/components/auth/auth.controller.ts
@@ -1,34 +1,74 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { validationResult } from 'express-validator';
 import { UserModel } from '../user/user.model';
 
-export function loginController(req: Request, res: Response) {
+export async function loginController(req: Request, res: Response) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+        return res.status(404).json({
+            error: `Пользователь с таким email не найден.`,
+        });
+    }
+
+    const isPasswordMatching: boolean = bcrypt.compareSync(req.body.password, user.password);
+
+    if (!isPasswordMatching) {
+        return res.status(401).json({
+            error: 'Пароли не совпадают. Попробуйте ещё раз.',
+        });
+    }
+
+    const secret: string = process.env.JWT_SECRET as string;
+    const expiresIn: number = 60 * 60;
+
+    const token = jwt.sign(
+        {
+            email: user.email,
+            userId: user._id,
+        },
+        secret,
+        { expiresIn },
+    );
+
     res.status(200).json({
-        login: 'from controller',
+        token: `Bearer ${token}`,
+        message: 'Login successful.',
     });
 }
 
 export async function registerController(req: Request, res: Response) {
-    const candidate = await UserModel.findOne({ email: req.body.email });
+    const errors = validationResult(req);
 
-    if (candidate) {
-        res.status(409).json({
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const isEmailExist = await UserModel.findOne({ email: req.body.email });
+
+    if (isEmailExist) {
+        return res.status(409).json({
             message: 'Такой email уже занят. Попробуйте другой.',
         });
-    } else {
-        const salt = bcrypt.genSaltSync(10);
-        const password = req.body.password;
+    }
 
-        const user = new UserModel({
-            email: req.body.email,
-            password: bcrypt.hashSync(password, salt),
-        });
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
 
-        try {
-            await user.save();
-            res.status(201).json(user);
-        } catch (error) {
-            console.error(error);
-        }
+    const user = new UserModel({ email: req.body.email, password });
+
+    try {
+        const savedUser = await user.save();
+        res.status(201).json({ data: savedUser, message: 'Register successful.' });
+    } catch (error) {
+        res.status(400).json({ error });
     }
 }
