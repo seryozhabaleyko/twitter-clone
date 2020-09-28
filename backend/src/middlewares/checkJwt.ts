153930ab6f8215fce38048843aa21d0bc258a14a
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-29 01:09:26 +0300

    Create checkJwt.ts

---
+++ backend/src/middlewares/checkJwt.ts
@@ -0,0 +1,25 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import { JWT_SECRET } from '../utils/secrets';
+import { UserModel } from '../components/user/user.model';
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: JWT_SECRET,
+};
+
+passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+        UserModel.findOne({ id: jwt_payload.sub }, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+                // or you could create a new account
+            }
+        });
+    }),
+);
