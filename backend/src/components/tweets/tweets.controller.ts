caa5bd90b35a8ef6e07e5c0002910b08e40d0b19
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-30 20:54:59 +0300

    Update tweets.controller.ts

--- backend/src/components/tweets/tweets.controller.ts
+++ backend/src/components/tweets/tweets.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
 import { ITweet } from './tweets.interface';
 import { TweetModel } from './tweets.model';
 import * as tweetsService from './tweets.service';
@@ -25,9 +26,15 @@ export async function getTweetById(req: Request, res: Response, next: NextFuncti
 
 export async function createTweet(req: Request, res: Response, next: NextFunction) {
     try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
         const tweet = await new TweetModel({
             text: req.body.text,
-            user: req.body.user,
+            user: req.user?._id,
         }).save();
 
         res.status(201).json({ tweet });
