cca88086ffe86899af3363b7b0ab84b82ba0c2ab
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-30 12:29:49 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,24 @@
 import express, { Application } from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+import logger from 'morgan';
 import passport from 'passport';
 import authRoute from './components/auth/auth.route';
-// import userRoute from './components/user/user.route';
+import usersRoute from './components/users/users.route';
+import tweetsRoute from './components/tweets/tweets.route';
 import './core/db';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 export const app: Application = express();
 
 app.use(passport.initialize());
 
-app.use(morgan('dev'));
+app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute); // passport.authenticate('jwt', { session: false })
+app.use('/api/tweets', tweetsRoute);
 
-// app.use('/api/user', passport.authenticate('jwt', { session: false }), userRoute);
+app.use(errorMiddleware);
