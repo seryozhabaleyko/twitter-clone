5e32ca1e30ea1bc754787ba7eb15c47f3e5c9885
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-09-30 20:55:12 +0300

    Update users.route.ts

--- backend/src/components/users/users.route.ts
+++ backend/src/components/users/users.route.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { createUser, getAllUsers, getByIdUser, removeUser, updateUser } from './users.controller';
+import * as usersController from './users.controller';
 
 const router = express.Router();
 
-router.get('/', getAllUsers);
-router.get('/:id', getByIdUser);
-router.post('/', createUser);
-router.delete('/:id', removeUser);
-router.patch('/:id', updateUser);
+router.get('/', usersController.getAllUsers);
+router.get('/:id', usersController.getByIdUser);
+router.post('/', usersController.createUser);
+router.delete('/:id', usersController.removeUser);
+router.patch('/:id', usersController.updateUser);
 
 export default router;
