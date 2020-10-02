0d00d15413bf9318037ace437d6b131d84b95677
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-02 21:51:09 +0300

    Create validateEnv.ts

---
+++ backend/src/utils/validateEnv.ts
@@ -0,0 +1,13 @@
+import { cleanEnv, port, str } from 'envalid';
+
+function validateEnv() {
+    cleanEnv(process.env, {
+        JWT_SECRET: str(),
+        MONGO_PASSWORD: str(),
+        MONGO_PATH: str(),
+        MONGO_USER: str(),
+        PORT: port(),
+    });
+}
+
+export { validateEnv };
