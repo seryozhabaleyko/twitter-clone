ee0b756ae6bd6556139d7414ce692ff027f94035
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-02 21:49:44 +0300

    Update tweets.route.ts

--- backend/src/components/tweets/tweets.route.ts
+++ backend/src/components/tweets/tweets.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import * as tweetsController from './tweets.controller';
 import { tweetsValidation } from './tweets.validation';
+import { upload } from './../../middlewares/uploads.middleware';
 
 const router = express.Router();
 
 router.get('/', tweetsController.getAllTweets);
 router.get('/:id', tweetsController.getTweetById);
-router.post('/', tweetsValidation, tweetsController.createTweet);
+router.post('/', upload.single('image'), tweetsValidation, tweetsController.createTweet);
 router.patch('/:id', tweetsController.updateTweet);
 router.delete('/:id', tweetsController.deleteTweet);
 
