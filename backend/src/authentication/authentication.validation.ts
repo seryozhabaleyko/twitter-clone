f37e361f729cafdc4afeb950b7039a36e5c193b7
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-05 17:02:39 +0300

    Update authentication.validation.ts

--- backend/src/authentication/authentication.validation.ts
+++ backend/src/authentication/authentication.validation.ts
@@ -1,4 +1,31 @@
-import { body } from 'express-validator';
+import { check } from 'express-validator';
 
-export const loginValidation = [body('email').isEmail(), body('password').isLength({ min: 5 })];
-export const registerValidation = [body('email').isEmail(), body('password').isLength({ min: 5 })];
+export const loginValidation = [
+    check('email')
+        .exists()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .normalizeEmail(),
+    check('password')
+        .exists()
+        .withMessage('Password is required')
+        .notEmpty()
+        .withMessage('Password must be filled')
+        .isLength({ min: 5 }),
+];
+
+export const registerValidation = [
+    check('email')
+        .exists()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .normalizeEmail(),
+    check('password')
+        .exists()
+        .withMessage('Password is required')
+        .notEmpty()
+        .withMessage('Password must be filled')
+        .isLength({ min: 5 }),
+];
