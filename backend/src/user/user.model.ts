60bb9b8db5994157315661dfc21e157d9ce8be90
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-05 17:03:54 +0300

    Update user.model.ts

--- backend/src/user/user.model.ts
+++ backend/src/user/user.model.ts
@@ -1,24 +1,36 @@
 import { Schema, model, Document } from 'mongoose';
 import { User } from './user.interface';
 
-const userSchema = new Schema({
-    name: String,
-    surname: String,
-    username: String,
-    email: {
-        type: String,
-        unique: true,
-        required: true,
+const userSchema = new Schema(
+    {
+        name: String,
+        surname: String,
+        username: String,
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    password: {
-        type: String,
-        required: true,
-    },
+    { timestamps: true },
+);
+
+userSchema.virtual('tweets', {
+    ref: 'Tweets',
+    localField: '_id',
+    foreignField: 'author',
 });
 
+userSchema.set('toObject', { virtuals: true });
 userSchema.set('toJSON', {
+    virtuals: true,
     transform(doc, ret) {
         delete ret.password;
+        delete ret.id;
         delete ret.__v;
     },
 });
