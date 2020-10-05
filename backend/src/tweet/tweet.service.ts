6e0945945d3c36cae90246d02a947196655ba8ad
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-05 17:03:43 +0300

    Update tweet.service.ts

--- backend/src/tweet/tweet.service.ts
+++ backend/src/tweet/tweet.service.ts
@@ -6,32 +6,46 @@ class TweetService {
     public tweet = tweetModel;
 
     public async getAllTweets(): Promise<Tweet[]> {
-        const tweets: Tweet[] = await this.tweet.find().populate('author', '-password');
+        const tweets: Tweet[] = await this.tweet.find().populate('author');
         return tweets;
     }
 
     public async getTweetById(id: string): Promise<Tweet> {
-        const tweet: Tweet | null = await this.tweet.findById(id);
-        if (!tweet) throw new TweetNotFoundException(id);
+        const tweet: Tweet | null = await this.tweet.findById(id).populate('author');
+
+        if (!tweet) {
+            throw new TweetNotFoundException(id);
+        }
+
         return tweet;
     }
 
     public async modifyTweet(id: string, data: Tweet): Promise<Tweet> {
-        const tweet: Tweet | null = await this.tweet.findByIdAndUpdate(id, data, { new: true });
-        if (!tweet) throw new TweetNotFoundException(id);
+        const tweet: Tweet | null = await this.tweet
+            .findByIdAndUpdate(id, data, { new: true })
+            .populate('author');
+
+        if (!tweet) {
+            throw new TweetNotFoundException(id);
+        }
+
         return tweet;
     }
 
-    public async createTweet(id: string, data: Tweet): Promise<Tweet> {
-        const createdTweet = new this.tweet({ ...data, author: id });
+    public async createTweet(authorId: string, data: Tweet): Promise<Tweet> {
+        const createdTweet = new this.tweet({ ...data, author: authorId });
         const savedTweet = await createdTweet.save();
-        const tweet = await savedTweet.populate('author', '-password').execPopulate();
+        const tweet = await savedTweet.populate('author').execPopulate();
         return tweet;
     }
 
-    public async deleteTweet(id: string): Promise<Tweet> {
-        const tweet: Tweet | null = await this.tweet.findByIdAndDelete(id);
-        if (!tweet) throw new TweetNotFoundException(id);
+    public async deleteTweet(tweetId: string): Promise<Tweet> {
+        const tweet: Tweet | null = await this.tweet.findByIdAndDelete(tweetId).populate('author');
+
+        if (!tweet) {
+            throw new TweetNotFoundException(tweetId);
+        }
+
         return tweet;
     }
 }
