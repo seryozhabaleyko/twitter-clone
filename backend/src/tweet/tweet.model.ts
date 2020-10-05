d6dcdc2830c3836cc09fee3e0df80952005d91e1
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-05 17:03:40 +0300

    Update tweet.model.ts

--- backend/src/tweet/tweet.model.ts
+++ backend/src/tweet/tweet.model.ts
@@ -1,11 +1,25 @@
 import { Schema, model, Document } from 'mongoose';
 import { Tweet } from './tweet.interface';
 
-const tweetSchema = new Schema({
-    text: String,
-    author: {
-        ref: 'Users',
-        type: Schema.Types.ObjectId,
+const tweetSchema = new Schema(
+    {
+        text: {
+            type: String,
+            trim: true,
+        },
+        author: {
+            ref: 'Users',
+            type: Schema.Types.ObjectId,
+        },
+    },
+    { timestamps: true, id: false },
+);
+
+tweetSchema.set('toObject', { virtuals: true });
+tweetSchema.set('toJSON', {
+    virtuals: true,
+    transform(doc, ret) {
+        delete ret.__v;
     },
 });
 
