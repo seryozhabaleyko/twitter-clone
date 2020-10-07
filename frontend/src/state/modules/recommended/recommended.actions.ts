f86d372c669412a6647f4405d20017fc2a1eb043
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-07 22:59:59 +0300

    Create recommended.actions.ts

---
+++ frontend/src/state/modules/recommended/recommended.actions.ts
@@ -0,0 +1,33 @@
+import {
+    Users,
+    FETCH_RECOMMENDED,
+    FETCH_RECOMMENDED_REQUEST,
+    FETCH_RECOMMENDED_SUCCESS,
+    FETCH_RECOMMENDED_FAILURE,
+} from './recommended.types';
+
+export function fetchRecommended() {
+    return {
+        type: FETCH_RECOMMENDED,
+    } as const;
+}
+
+export function fetchRecommendedRequest() {
+    return {
+        type: FETCH_RECOMMENDED_REQUEST,
+    } as const;
+}
+
+export function fetchRecommendedSuccess(users: Users) {
+    return {
+        type: FETCH_RECOMMENDED_SUCCESS,
+        payload: users,
+    } as const;
+}
+
+export function fetchRecommendedFailure(error: Error) {
+    return {
+        type: FETCH_RECOMMENDED_FAILURE,
+        payload: error,
+    } as const;
+}
