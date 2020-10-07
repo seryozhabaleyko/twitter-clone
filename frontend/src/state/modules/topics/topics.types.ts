1ef62446e241a996a4a27eb14b1a30df8acfdfc7
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-07 23:01:02 +0300

    Create topics.types.ts

---
+++ frontend/src/state/modules/topics/topics.types.ts
@@ -0,0 +1,32 @@
+import {
+    fetchTopics,
+    fetchTopicsFailure,
+    fetchTopicsRequest,
+    fetchTopicsSuccess,
+} from './topics.actions';
+
+export const FETCH_TOPICS = 'FETCH_TOPICS';
+export const FETCH_TOPICS_REQUEST = 'FETCH_TOPICS_REQUEST';
+export const FETCH_TOPICS_SUCCESS = 'FETCH_TOPICS_SUCCESS';
+export const FETCH_TOPICS_FAILURE = 'FETCH_TOPICS_FAILURE';
+
+export interface Topic {
+    topic: string;
+    name: string;
+    tweet_value: number;
+}
+
+export type Topics = Topic[];
+
+export interface TopicsState {
+    loading: boolean;
+    data: Topics;
+    error: Error | null;
+}
+
+export type FetchTopics = ReturnType<typeof fetchTopics>;
+
+export type TopicsActionTypes =
+    | ReturnType<typeof fetchTopicsRequest>
+    | ReturnType<typeof fetchTopicsSuccess>
+    | ReturnType<typeof fetchTopicsFailure>;
