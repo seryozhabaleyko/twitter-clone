22ace821f32c58b171d875b334f93e09327113cd
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-07 23:01:12 +0300

    Create tweet.types.ts

---
+++ frontend/src/state/modules/tweet/tweet.types.ts
@@ -0,0 +1,49 @@
+import {
+    addTweet,
+    addTweetRequest,
+    addTweetSuccess,
+    addTweetFailure,
+    fetchTweet,
+    fetchTweetRequest,
+    fetchTweetSuccess,
+    fetchTweetFailure,
+} from './tweet.actions';
+
+export const ADD_TWEET = 'ADD_TWEET';
+export const ADD_TWEET_REQUEST = 'ADD_TWEET_REQUEST';
+export const ADD_TWEET_SUCCESS = 'ADD_TWEET_SUCCESS';
+export const ADD_TWEET_FAILURE = 'ADD_TWEET_FAILURE';
+
+export const FETCH_TWEET = 'FETCH_TWEET';
+export const FETCH_TWEET_REQUEST = 'FETCH_TWEET_REQUEST';
+export const FETCH_TWEET_SUCCESS = 'FETCH_TWEET_SUCCESS';
+export const FETCH_TWEET_FAILURE = 'FETCH_TWEET_FAILURE';
+
+export interface Tweet {
+    _id: string;
+    text: string;
+    author: {
+        name: string;
+        surname: string;
+        username: string;
+        avatarUrl: string;
+    };
+}
+
+export interface TweetState {
+    loading: boolean;
+    data: Tweet;
+    error: Error | null;
+}
+
+export type AddTweet = ReturnType<typeof addTweet>;
+
+export type FetchTweet = ReturnType<typeof fetchTweet>;
+
+export type TweetsActionTypes =
+    | ReturnType<typeof addTweetRequest>
+    | ReturnType<typeof addTweetSuccess>
+    | ReturnType<typeof addTweetFailure>
+    | ReturnType<typeof fetchTweetRequest>
+    | ReturnType<typeof fetchTweetSuccess>
+    | ReturnType<typeof fetchTweetFailure>;
