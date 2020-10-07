6f5f29deb33e3210d91f225f9279f8612a29095b
seryozhabaleyko <seryozha.baleyko@gmail.com>
2020-10-07 23:01:08 +0300

    Create tweet.saga.ts

---
+++ frontend/src/state/modules/tweet/tweet.saga.ts
@@ -0,0 +1,38 @@
+import { takeEvery, put, call } from 'redux-saga/effects';
+import { AddTweet, ADD_TWEET, FetchTweet, FETCH_TWEET } from './tweet.types';
+import {
+    addTweetRequest,
+    addTweetSuccess,
+    addTweetFailure,
+    fetchTweetRequest,
+    fetchTweetSuccess,
+    fetchTweetFailure,
+} from './tweet.actions';
+import { addTweetApi, fetchTweetApi } from '../../../services/api';
+
+function* addTweet({ tweet }: AddTweet) {
+    try {
+        yield put(addTweetRequest());
+        const response = yield call(addTweetApi, tweet);
+        console.log('tweet response', response);
+
+        yield put(addTweetSuccess(response.data));
+    } catch (error) {
+        yield put(addTweetFailure(error));
+    }
+}
+
+function* fetchTweet({ id }: FetchTweet) {
+    try {
+        yield put(fetchTweetRequest());
+        const tweet = yield call(fetchTweetApi, id);
+        yield put(fetchTweetSuccess(tweet[0]));
+    } catch (error) {
+        yield put(fetchTweetFailure(error));
+    }
+}
+
+export function* tweetSaga() {
+    yield takeEvery(ADD_TWEET, addTweet);
+    yield takeEvery(FETCH_TWEET, fetchTweet);
+}
